7a5a80f9772e2ce30c688504cf609900166fbd4e
1kingsmen <autaayenajeyi@gmail.com>
2026-04-17 21:00:26 +0100

    worked on the menu section

--- scripts/cart.js
+++ scripts/cart.js
@@ -3,3 +3,9 @@ const orderBtn = document.querySelector('.order');
 orderBtn.addEventListener('click', () => {
     window.location.href = 'menu.html';
 });
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const saved = localStorage.getItem("cart")
+    const cartObj = JSON.parse(saved)
+    console.log(cartObj)
+})
